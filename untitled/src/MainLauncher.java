00cb7398a05e14163b1f8fc6e485826c65a637e4
mayanklitt <mayankkumar1@dcmail.ca>
2024-08-15 23:49:07 -0400

    Update main launcher

--- untitled/src/MainLauncher.java
+++ untitled/src/MainLauncher.java
@@ -1,3 +1,9 @@
+// Author : Mayank kumar and Kultaran Singh
+// Name: MainLauncher.java
+// Version : 1.0
+// Date: 10/08/2024
+// Description : This code file create task manager app using Java Swing UI
+
 import javax.swing.*;
 
 public class MainLauncher {
